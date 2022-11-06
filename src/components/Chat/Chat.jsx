3915cbccef1a1a1c9fc8c80d9fd0ebec1b708f71
Saharsh Agrawal <saharshagrawal@berkeley.edu>
2022-11-06 08:04:04 -0800

    Adding instructions to chat tab

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -2,9 +2,18 @@ import React, { FC, ReactElement, Component, Fragment, useState } from 'react'
 
   const Chat = () => {
     return (
-      <div className="flex flex-col items-center mt-20 text-sm">
+      <div className="flex flex-col items-center mt-10 text-sm">
         <a href="https://xmtp.vercel.app/">Visit <span class="font-bold">https://xmtp.vercel.app/</span> to view chat client</a>
+      
+        <div className="px-3 py-5">
+          <a>To join the Decentraland GameChat Server, simply send a <span class="font-bold">/join</span> message request to the GameChat <span class="font-bold">server address</span> viewable on the Game tab.</a>
         </div>
+
+        {/* <div className="px-10 py-5">
+          For example, try sending...
+        </div> */}
+      
+      </div>
     )
   }
 
