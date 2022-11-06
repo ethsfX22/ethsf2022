22fed6fff60a480e93fa7a41ddcb6eb6a37b025d
Saharsh Agrawal <saharshagrawal@berkeley.edu>
2022-11-06 07:51:21 -0800

    Integrating wallet connect button

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -8,8 +8,6 @@ import Profile from '../../components/Profile/Profile';
 import './Popup.css';
 
 import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid';
-import { Combobox } from '@headlessui/react';
-import { useWallet } from './WalletProvider';
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
@@ -17,18 +15,10 @@ function classNames(...classes) {
 
 const Popup = () => {
   const [open, setOpen] = useState(true);
-  const { isAuthenticated, connectWallet, disconnectWallet } = useWallet();
 
   return (
     <div className="App">
       <header className="App-header"></header>
-      <button
-        onClick={isAuthenticated ? disconnectWallet : connectWallet}
-        id="wallet-connect"
-      >
-        Button For User
-        {isAuthenticated ? 'Disconnect Wallet' : 'Connect Wallet'}
-      </button>
       <Profile open={open} setOpen={setOpen} />
       <Tabs open={open} setOpen={setOpen} />
     </div>
